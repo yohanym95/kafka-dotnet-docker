d55c05a1d5ae2b2ce87229ecc17a9908af2d812b
unknown <malshikay@gmail.com>
2026-04-25 13:57:27 +0800

    Add Intial projects

---
+++ KafkaConsumer/KafkaConsumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+using System;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "order-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+consumer.Subscribe("demo-topic");
+Console.WriteLine("Waiting for events...");
+try
+{
+    while (true)
+    {
+        var result = consumer.Consume();
+        Console.WriteLine($"Received: {result.Message.Value}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    consumer.Close();
+}
---
+++ KafkaProducer/KafkaProducer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+Console.WriteLine("Sending order event...");
+try
+{
+    var result = await producer.ProduceAsync(
+        "demo-topic",
+        new Message<Null, string>
+        {
+            Value = "Order Created"
+        });
+    Console.WriteLine($"Sent: {result.TopicPartitionOffset}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
